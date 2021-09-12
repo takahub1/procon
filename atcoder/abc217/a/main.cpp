eabbab98fcaeeb6d1fe018ed4aaabdda3879d083
takahub1 <you@example.com>
2021-09-12 12:05:44 +0900

    add abc217

---
+++ atcoder/abc217/a/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+ 
+using namespace std;
+typedef long long ll;
+#define vi vector<int>
+#define vll vector<long long>
+ 
+int main(void){
+	string s,t;
+	cin >> s >> t;
+	if (s < t){
+		cout << "Yes";
+	}
+	else{
+		cout << "No";
+	}
+	// cout << s << endl;
+	// cout << t << endl;
+ 
+	return 0;
+}
